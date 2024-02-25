1eeeb737a541e5530189340e5008643dc2c63ad1
Draen <67590171+ThatDraenGuy@users.noreply.github.com>
2024-02-25 17:33:10 +0300

    chore: fix arccos test coverage and formatting (#4)

--- function/src/main/java/thatdraenguy/Arccos.java
+++ function/src/main/java/thatdraenguy/Arccos.java
@@ -1,8 +1,10 @@
 package thatdraenguy;
 
-
 public class Arccos {
     private static final int MAX_PRECISION_DECIMALS = 6;
+
+    private Arccos() {
+    }
 
     public static double calculate(double point, int precisionDecimals) {
         if (point < -1. || point > 1.) {
@@ -12,7 +14,7 @@ public class Arccos {
         if (precisionDecimals > MAX_PRECISION_DECIMALS) {
             throw new IllegalArgumentException("Cannot calcualte with precision bigger than 6 decimal points!");
         }
-        
+
         if (precisionDecimals <= 0)
             precisionDecimals = 0;
 
@@ -36,15 +38,14 @@ public class Arccos {
 
         if (isNegative)
             result = Math.PI - result;
-            
 
         return result;
     }
 
     private static double getTerm(int index, double point, boolean isNearZero) {
         double num = isNearZero ? Math.pow(point, 2 * index + 1) : Math.pow(2, -index) * Math.pow(1 - point, index);
-        return num * getFactorial(2 * index) /
-            ( Math.pow(2, 2 * index) * Math.pow(getFactorial(index), 2) * ( 2 * index + 1 ) );
+        return num * getFactorial(2 * index)
+                / (Math.pow(2, 2 * index) * Math.pow(getFactorial(index), 2) * (2 * index + 1));
     }
 
     private static int getFactorial(int num) {
