19a9198be6510ac9c3cc5703d212921a3f1b555b
Draen <67590171+ThatDraenGuy@users.noreply.github.com>
2024-03-01 14:06:30 +0300

    test: use parametrized tests in function (#7)

--- function/src/test/java/thatdraenguy/ArccosTest.java
+++ function/src/test/java/thatdraenguy/ArccosTest.java
@@ -1,74 +1,69 @@
 package thatdraenguy;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.stream.Stream;
+
 public class ArccosTest {
-    @Test
-    public void illegalPoint() {
+    @ParameterizedTest
+    @ValueSource(doubles = { 1.01, 178, -1.01, -291 })
+    public void illegalPoint(double point) {
         assertThrows(IllegalArgumentException.class, () -> {
-            Arccos.calculate(1.01, 1);
-        });
-        assertThrows(IllegalArgumentException.class, () -> {
-            Arccos.calculate(178, 1);
-        });
-        assertThrows(IllegalArgumentException.class, () -> {
-            Arccos.calculate(-1.01, 1);
-        });
-        assertThrows(IllegalArgumentException.class, () -> {
-            Arccos.calculate(-291, 1);
+            Arccos.calculate(point, 1);
         });
     }
 
-    @Test
-    public void illegalPrecisionDecimals() {
+    @ParameterizedTest
+    @ValueSource(ints = { 20, 10, 14, 100 })
+    public void illegalPrecisionDecimals(int precisionDecimals) {
         assertThrows(IllegalArgumentException.class, () -> {
-            Arccos.calculate(0, 20);
+            Arccos.calculate(0, precisionDecimals);
         });
         assertThrows(IllegalArgumentException.class, () -> {
-            Arccos.calculate(0, 10);
-        });
-        assertThrows(IllegalArgumentException.class, () -> {
-            Arccos.calculate(-0.4, 10);
-        });
-        assertThrows(IllegalArgumentException.class, () -> {
-            Arccos.calculate(0.2, 10);
+            Arccos.calculate(-0.95, precisionDecimals);
         });
     }
 
-    @Test
-    public void correctArguments() {
+    @ParameterizedTest
+    @MethodSource
+    public void correctArguments(double[] args) {
         assertDoesNotThrow(() -> {
-            Arccos.calculate(0, 1);
-            Arccos.calculate(1, 1);
-            Arccos.calculate(-1, 1);
-            Arccos.calculate(0.259269524, 1);
-            Arccos.calculate(-0.259269524, 1);
-            Arccos.calculate(0.562, 5);
-            Arccos.calculate(-0.91, 6);
-            Arccos.calculate(0.83, 0);
-            Arccos.calculate(0.25, -4);
+            Arccos.calculate(args[0], (int) args[1]);
         });
     }
 
-    @Test
-    public void resultValues() {
-        double[] points = new double[] { -1, -0.97, -0.83, -0.753, -0.6, -0.51, -0.43, -0.37, -0.22, -0.156, 0, 0.156,
-                0.22, 0.37, 0.43, 0.51, 0.6, 0.753, 0.83, 0.97, 1 };
-        double[] results = new double[] { 3.141592653589793, 2.896027136074501, 2.549904011163249, 2.4234057184577593,
-                2.214297435588181, 2.1059811170704963, 2.0152891037307157, 1.9498053474908474, 1.792610797291691,
-                1.7274360940808284, 1.5707963267948966, 1.414156559509, 1.3489818562981022, 1.191787306098946,
-                1.1263035498590777, 1.0356115365192968, 0.9272952180016123, 0.7181869351320339, 0.5916886424265441,
-                0.24556551751529213, 0 };
+    private static Stream<double[]> correctArguments() {
+        return Stream.of(new double[] { 0, 1 }, new double[] { 1, 1 }, new double[] { -1, 1 },
+                new double[] { 0.259269524, 1 }, new double[] { -0.259269524, 1 }, new double[] { 0.562, 5 },
+                new double[] { -0.91, 6 }, new double[] { 0.83, 0 }, new double[] { 0.25, -4 });
+    }
 
-        for (int point_index = 0; point_index < points.length; point_index++) {
-            double point = points[point_index];
-            double result = results[point_index];
+    @ParameterizedTest
+    @MethodSource
+    public void resultValues(double[] values) {
+        double point = values[0];
+        double result = values[1];
 
-            for (int precision = 0; precision < 7; precision++) {
-                assertEquals(result, Arccos.calculate(point, precision), Math.pow(10, -precision));
-            }
+        for (int precision = 0; precision < 7; precision++) {
+            assertEquals(result, Arccos.calculate(point, precision), Math.pow(10, -precision));
         }
+    }
+
+    private static Stream<double[]> resultValues() {
+        return Stream.of(new double[] { -1, 3.141592653589793 }, new double[] { -0.97, 2.896027136074501 },
+                new double[] { -0.83, 2.549904011163249 }, new double[] { -0.753, 2.4234057184577593 },
+                new double[] { -0.6, 2.214297435588181 }, new double[] { -0.51, 2.1059811170704963 },
+                new double[] { -0.43, 2.0152891037307157 }, new double[] { -0.37, 1.9498053474908474 },
+                new double[] { -0.22, 1.792610797291691 }, new double[] { -0.156, 1.7274360940808284 },
+                new double[] { 0, 1.5707963267948966 }, new double[] { 0.156, 1.414156559509 },
+                new double[] { 0.22, 1.3489818562981022 }, new double[] { 0.37, 1.191787306098946 },
+                new double[] { 0.43, 1.1263035498590777 }, new double[] { 0.51, 1.0356115365192968 },
+                new double[] { 0.6, 0.9272952180016123 }, new double[] { 0.753, 0.7181869351320339 },
+                new double[] { 0.83, 0.5916886424265441 }, new double[] { 0.97, 0.24556551751529213 },
+                new double[] { 1, 0 });
     }
 }
